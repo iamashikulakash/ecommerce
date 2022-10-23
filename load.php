a6a857056a6363ce95656b79f2ced94c69677aa2
Ashikul Islam Akash <51879563+iamashikulakash@users.noreply.github.com>
2022-10-23 14:07:32 +0600

    Add files via upload

---
+++ load.php
@@ -0,0 +1,26 @@
+<!-- CODE BY ASHIKUL ISLAM AKASH @iamashikkulakash -->
+<?php
+
+session_start();
+
+include("includes/db.php");
+
+include("functions/functions.php");
+
+switch($_REQUEST['sAction']){
+
+default :
+
+getProducts();
+
+break;
+
+case'getPaginator';
+
+getPaginator();
+
+break;
+
+}
+
+?>
---
+++ logout.php
@@ -0,0 +1,11 @@
+<!-- CODE BY ASHIKUL ISLAM AKASH @iamashikkulakash -->
+<?php
+
+session_start();
+
+session_destroy();
+
+echo "<script>window.open('index.php','_self')</script>";
+
+
+?>
